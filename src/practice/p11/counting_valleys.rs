16b404ece18f158b7caac628848026bc699cbe2e
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2024-10-22 10:52:57 +0300

    -- practice 11 refactor

--- src/practice/p11/counting_valleys.rs
+++ src/practice/p11/counting_valleys.rs
@@ -1,5 +1,6 @@
 ///  https://www.hackerrank.com/challenges/counting-valleys/problem
 fn countingValleys1(steps: i32, path: &str) -> i32 {
+    #[derive(Clone)]
     struct State {
         level: i32,
         count: i32,
@@ -12,27 +13,28 @@ fn countingValleys1(steps: i32, path: &str) -> i32 {
         fn fresh() -> Self {
             Self::new(0, 0)
         }
-        fn calc_delta(prev_level: i32, curr_level: i32) -> i32 {
-            match curr_level {
-                0 if prev_level < 0 => 1,
-                _ => 0,
+        fn delta(prev_level: i32, curr_level: i32) -> i32 {
+            if curr_level == 0 && prev_level < 0 {
+                1
+            } else {
+                0
             }
         }
         fn up(&self) -> Self {
             let curr_level = self.level + 1;
-            let delta_count = Self::calc_delta(self.level, curr_level);
+            let delta_count = Self::delta(self.level, curr_level);
             State::new(curr_level, self.count + delta_count)
         }
         fn down(&self) -> Self {
             let curr_level = self.level - 1;
-            let delta_count = Self::calc_delta(self.level, curr_level);
+            let delta_count = Self::delta(self.level, curr_level);
             State::new(curr_level, self.count + delta_count)
         }
         fn process(&self, c: char) -> State {
             match c {
                 'U' => self.up(),
                 'D' => self.down(),
-                _ => panic!("wrong char"),
+                _ => self.clone(), // or without clone, by moving. remove & in `up` and `down`
             }
         }
     }
@@ -51,12 +53,10 @@ fn countingValleys2(steps: i32, path: &str) -> i32 {
             'D' => prev_level - 1,
             _ => prev_level,
         };
-        let delta = match curr_level {
-            0 if prev_level < 0 => 1,
-            _ => 0,
-        };
+        if curr_level == 0 && prev_level < 0 {
+            count += 1;
+        }
         prev_level = curr_level;
-        count += delta;
     }
     count
 }
